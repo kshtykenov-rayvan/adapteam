686753edd83d4712cae3cf750543a8c81e8133ef
rayvan <kshtykenov.rayvan@gmail.com>
2024-09-24 11:43:40 +0500

    img set fun created

--- img/edit_fun.py
+++ img/edit_fun.py
@@ -1,22 +1,30 @@
 from PIL import Image, ImageEnhance
-import numpy as np
 
 
 # Функция для обработки изображения
-def process_image(input_path, output_path):
+def process_image(input_path, output_path, brightness_factor=0.5, contrast_factor=3, exposure_steps=1):
     # Открываем изображение
     image = Image.open(input_path)
 
+    # Увеличиваем яркость
+    enhancer = ImageEnhance.Brightness(image)
+    image = enhancer.enhance(brightness_factor)  # Коэффициент яркости (1.0 - оригинал)
+
     # Увеличиваем четкость
-    enhancer = ImageEnhance.Sharpness(image)
-    image = enhancer.enhance(2.0)  # 2.0 - коэффициент увеличения четкости
+    sharpness_enhancer = ImageEnhance.Sharpness(image)
+    image = sharpness_enhancer.enhance(2.0)  # Коэффициент увеличения четкости
 
     # Преобразуем изображение в черно-белое
     image = image.convert("L")
 
     # Увеличиваем контраст
     contrast_enhancer = ImageEnhance.Contrast(image)
-    image = contrast_enhancer.enhance(1.5)  # 1.5 - коэффициент увеличения контраста
+    image = contrast_enhancer.enhance(contrast_factor)  # Коэффициент увеличения контраста
+
+    # Увеличиваем экспозицию
+    # Экспозиция может быть реализована через изменение значения пикселей
+    if exposure_steps > 0:
+        image = image.point(lambda p: min(p * (2 ** exposure_steps), 255))
 
     # Сохраняем обработанное изображение
     image.save(output_path)
@@ -24,5 +32,11 @@ def process_image(input_path, output_path):
 
 # Пример использования функции
 input_image_path = 'original/0001.jpg'  # путь к исходному изображению
-output_image_path = 'edit/output1.jpg'  # путь для сохранения обработанного изображения
-process_image(input_image_path, output_image_path)
+output_image_path = 'edit/0001.jpg'  # путь для сохранения обработанного изображения
+
+# Пример параметров
+brightness_factor = 0.5  # Яркость +20%
+contrast_factor = 3  # Контраст +25%
+exposure_steps = 1  # Экспозиция +1 ступень
+
+process_image(input_image_path, output_image_path, brightness_factor, contrast_factor, exposure_steps)
